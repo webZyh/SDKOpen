24440f18793b6f8ff399c66164ea29211fc7c9d7
zyh <jt_zyh10000@163.com>
2019-04-24 17:38:03 +0800

    eCharts

--- web/src/views/Chart/Chart.vue
+++ web/src/views/Chart/Chart.vue
@@ -1,27 +1,88 @@
 <template>
-    <div class="">
-      <button v-on:click="show = !show">
-        Toggle
-      </button>
-      <transition name="fade">
-        <p v-if="show">hello</p>
-      </transition>
-    </div>
+  <div class="">
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <div :class="className" :style="{height:height,width:width}" ref="myChart"></div>
+    </el-row>
+  </div>
 </template>
 <script>
+  import echarts from 'echarts'
   export default {
-    data(){
-      return{
-        show: true
+    props: {
+      className: {
+        type: String,
+        default: 'chart'
+      },
+      width: {
+        type: String,
+        default: '100%'
+      },
+      height: {
+        type: String,
+        default: '350px'
+      },
+    },
+    data() {
+      return {
+        chart: null
       }
+    },
+    mounted(){
+      this.initChart()
+    },
+    methods:{
+      initChart(){
+        this.chart = echarts.init(this.$refs.myChart);
+        //此处可以获取异步数据
+        this.chart.setOption({
+          color: ['#3398DB'],
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [{
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+              alignWithLabel: true
+            }
+          }],
+          yAxis: [{
+            type: 'value'
+          }],
+          series: [{
+            name: '直接访问',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+          }]
+        });
+      }
+    },
+    beforeDestroy() {
+      if (!this.chart) {
+        return
+      }
+      this.chart.dispose();
+      this.chart = null
     }
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
   .fade-enter-active, .fade-leave-active {
-    transition: all  .5s;
+    transition: all .5s;
   }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+  {
     margin-left: 100px;
   }
 </style>
